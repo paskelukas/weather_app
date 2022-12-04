b9730afeec82d41f173c38d65ffe81ba416098c6
LukasVilniusTech <119747613+LukasVilniusTech@users.noreply.github.com>
2022-12-04 02:16:18 +0200

    Adding app functionality

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -4,3 +4,56 @@ const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
+const updateUI = (data) => {
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span> 
+        </div>
+    `;
+
+    //update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if(weather.IsDayTime) {
+        timeSrc = 'img/day.svg';
+    } else {
+        timeSrc = 'img/night.svg';
+    }
+
+    //remove the d-none class if present
+    if(card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+};
+
+const place = async city => {
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {
+        cityDetails: cityDetails,
+        weather: weather
+    };
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the UI with the new city
+    place(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
